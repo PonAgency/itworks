38240b4aa9201b85e00cd36d4737d5b00ece19be
PonAgency <contact@pon-agency.com>
2022-02-02 10:27:25 +0100

    devise works

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
+
+  def index
+    @user = User.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to_root_path
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+end
